2e75ee53c661345504ec967003ffa4d3e7f22630
Brennan McMicking <brennanmcmicking@gmail.com>
2023-06-20 01:10:20 -0700

    Add basic resources and grant permissions

--- lib/spotify-service-stack.ts
+++ lib/spotify-service-stack.ts
@@ -1,16 +1,35 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+const SECRET_ARN = "arn:aws:secretsmanager:us-west-2:446708209687:secret:spotify/refresh-token-FetKWZ";
 
 export class SpotifyServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const stage = props?.description;
 
-    // The code that defines your stack goes here
+    const secret = Secret.fromSecretCompleteArn(this, "SpotifyRefreshToken", "arn:aws:secretsmanager:us-west-2:446708209687:secret:spotify/refresh-token-FetKWZ");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SpotifyServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const handler = new Function(this, `NowPlayingHandler-${stage}`, {
+      runtime: Runtime.PYTHON_3_10,
+      code: Code.fromAsset("resources"),
+      handler: "nowplaying.handler",
+      environment: {
+        REFRESH_TOKEN: SECRET_ARN,
+      }
+    });
+
+    secret.grantRead(handler);
+
+    const api = new LambdaRestApi(this, `NowPlayingApi-${stage}`, {
+      handler,
+      proxy: false,
+    });
+
+    const now_playing = api.root.addResource("now-playing");
+    now_playing.addMethod('GET');
   }
 }
