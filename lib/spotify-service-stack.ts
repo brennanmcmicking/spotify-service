75b334a98321c7c038f5386d5e471bc7fbdfb24d
Brennan McMicking <brennanmcmicking@gmail.com>
2023-06-20 03:54:15 -0700

    Fix lambda code to successfully refresh credentials

--- lib/spotify-service-stack.ts
+++ lib/spotify-service-stack.ts
@@ -4,25 +4,29 @@ import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 
-const SECRET_ARN = "arn:aws:secretsmanager:us-west-2:446708209687:secret:spotify/refresh-token-FetKWZ";
+const REFRESH_TOKEN_ARN = "arn:aws:secretsmanager:us-west-2:446708209687:secret:spotify/refresh-token-FetKWZ";
+const CLIENT_SECRET_ARN = "arn:aws:secretsmanager:us-west-2:446708209687:secret:spotify/client-secret-XZfhGY";
 
 export class SpotifyServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const stage = props?.description;
 
-    const secret = Secret.fromSecretCompleteArn(this, "SpotifyRefreshToken", "arn:aws:secretsmanager:us-west-2:446708209687:secret:spotify/refresh-token-FetKWZ");
+    const refresh_secret = Secret.fromSecretCompleteArn(this, "SpotifyRefreshToken", REFRESH_TOKEN_ARN);
+    const client_secret = Secret.fromSecretCompleteArn(this, "SpotifyClientSecret", CLIENT_SECRET_ARN);
 
     const handler = new Function(this, `NowPlayingHandler-${stage}`, {
       runtime: Runtime.PYTHON_3_10,
       code: Code.fromAsset("resources"),
       handler: "nowplaying.handler",
       environment: {
-        REFRESH_TOKEN: SECRET_ARN,
+        REFRESH_TOKEN_ARN,
+        CLIENT_SECRET_ARN,
       }
     });
 
-    secret.grantRead(handler);
+    refresh_secret.grantRead(handler);
+    client_secret.grantRead(handler);
 
     const api = new LambdaRestApi(this, `NowPlayingApi-${stage}`, {
       handler,
--- resources/nowplaying.py
+++ resources/nowplaying.py
@@ -1,14 +1,62 @@
 import os
 import json
+import base64
+import datetime
+
+import urllib3
+http = urllib3.PoolManager()
+import botocore 
+import botocore.session 
+# from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
+
+secrets_client = botocore.session.get_session().create_client('secretsmanager')
+# cache_config = SecretCacheConfig()
+# cache = SecretCache( config = cache_config, client = client)
+
+ACCOUNTS_URL = "https://accounts.spotify.com/api/token"
+API_URL = "https://api.spotify.com/v1/me/player/currently-playing"
+
+REFRESH_TOKEN = secrets_client.get_secret_value(SecretId=os.environ['REFRESH_TOKEN_ARN'])["SecretString"]
+CLIENT_SECRET = base64.b64encode(secrets_client.get_secret_value(SecretId=os.environ['CLIENT_SECRET_ARN'])["SecretString"].encode('ascii')).decode('ascii')
+print("Refreshed secrets")
+
+
+def refreshAccessToken():
+  res = http.request("POST", ACCOUNTS_URL, 
+    headers={
+      'Authorization': f'Basic {CLIENT_SECRET}',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body=f"grant_type=refresh_token&refresh_token={REFRESH_TOKEN}"
+  )
+  print(res.data)
+  return json.loads(res.data)["access_token"]
+
+
+access_token = refreshAccessToken()
+access_token_last_update = datetime.datetime.now()
+print("Got first access token")
+
 
 def handler(event, context):
-  json_region = os.environ['AWS_REGION']
-  return {
-    "statusCode": 200,
-    "headers": {
-        "Content-Type": "application/json"
-    },
-    "body": json.dumps({
-        "Region ": json_region
-    })
+  global access_token
+  global access_token_last_update
+  if access_token is None or datetime.datetime.now() - access_token_last_update > datetime.timedelta(minutes=59):
+    access_token = refreshAccessToken()
+    access_token_last_update = datetime.datetime.now()
+    print("Refreshed access token")
+
+  headers = {
+    'Authorization': f'Bearer {access_token}'
   }
+
+  r = http.request('GET', API_URL, headers=headers)
+
+  print(r.data)
+  resJson = json.loads(r.data)
+  print(resJson)
+
+  return {
+        "statusCode": 200,
+        "body": r.data,
+    }
